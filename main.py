0be2b860153eb6359211fc6812e35f550545b9da
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-01-09 15:07:14 +0100

    Fixed summary display

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from matplotlib.axes import Axes
 
 from genetics import Evaluator
 
-def main(filename: str = 'lagerstatus.csv', vocal: bool = True) -> None:
+def main(filename: str = 'lagerstatus2.csv', vocal: bool = True) -> None:
     N_TRUCKS = 10
     TRUCK_CAPACITY = 800
+    MAX_MINUTES = 60
 
     data_path = Path(filename)
     data_array = np.genfromtxt(
@@ -22,34 +23,18 @@ def main(filename: str = 'lagerstatus.csv', vocal: bool = True) -> None:
         n_positions = N_TRUCKS,
         position_capacity = TRUCK_CAPACITY
     )
-    # evaluator.evaluate(
-    #         population_size = 100,
-    #         repetition_limit = 500,
-    #         minimum_growth = 0.1,
-    #         vocal = vocal
-    #     )
-    # evaluator.population.append(evaluator._random_genome()) # TODO: REMOVE
-    from collections import defaultdict
-    first_positive = defaultdict(int)
-    for iteration in range(1000):
-        if iteration % 50 == 0:
-            print(iteration)
-        first_positive[evaluator.evaluate(
+    evaluator.evaluate(
             population_size = 100,
-            repetition_limit = 500,
+            repetition_limit = 1000,
             minimum_growth = 0.1,
-            vocal = vocal
-        )] += 1
+            vocal = vocal,
+            max_minutes= MAX_MINUTES
+        )
+    evaluator.population.append(evaluator._random_genome()) # TODO: REMOVE
     
-    vocal = False
-    first_positives = [(key, value) for key, value in first_positive.items()]
-    for key, value in sorted(first_positives, key=lambda x:x[0]):
-        print(f'Gen {key}: {value}')
-        
-
     statistics = [
-        {'name': 'weights', 'data': data_array[:, 1]},
-        {'name': 'earnings', 'data': data_array[:, 2]}
+        {'name': 'Weights', 'data': data_array[:, 1]},
+        {'name': 'Earnings', 'data': data_array[:, 2]}
     ]
 
     _, ax = plt.subplots(1, 2)
@@ -59,42 +44,48 @@ def main(filename: str = 'lagerstatus.csv', vocal: bool = True) -> None:
         std = np.std(stat['data'])
         var = np.var(stat['data'])
         ax[index].hist(stat['data'])
-        ax[index].set_title(stat['name'].capitalize())
+        ax[index].set_title(stat['name'])
         ax[index].axvline(x=mean, color='k', label=f'Mean')
         ax[index].axvline(x=mean-std, color='k', linestyle='dotted', label=f'Standard deviation')
         ax[index].axvline(x=mean+std, color='k', linestyle='dotted')
         ax[index].plot([], [], ' ', label=f'Variance: {var:.2f}')
         ax[index].legend(loc='upper left')
     
+    best_load = evaluator.population[0]
+    load_array = np.array(best_load).reshape(len(best_load), -1)
+    merged_array = np.hstack((evaluator.data, load_array))
+    
+    loaded_mask = (merged_array[:, 4] != 0)
+    remaining_mask = (merged_array[:, 4] == 0)
+    penalty_mask = (merged_array[:, 3] < 0)
+    
     if vocal:
-        best_load = evaluator.population[0]
-        load_array = np.array(best_load).reshape(len(best_load), -1)
-        merged_array = np.hstack((evaluator.data, load_array))
-
-        loaded_mask = (merged_array[:, 4] != 0)
-        remaining_mask = (merged_array[:, 4] == 0)
-        overdue_mask = (merged_array[:, 3] < 0)
-
-        total_packages = 0
+        print('=====================================================')
+        delivery_packages = 0
         for truck_id in range(1, evaluator.n_positions + 1):
             truck_mask = (merged_array[:, 4] == truck_id)
             truck_packages = merged_array[truck_mask, 1]
             truck_weight = sum(truck_packages)
-            total_packages += len(truck_packages)
+            delivery_packages += len(truck_packages)
 
             message = f'Truck #{truck_id}: '
             message += f'{truck_weight:.2f}/{TRUCK_CAPACITY} kgs loaded, '
             message += f'{len(truck_packages)} packages.'
             print(message)
+        print('=====================================================')
         
-        remaining_overdue = len(merged_array[overdue_mask & remaining_mask])
-        daily_earnings = sum(merged_array[loaded_mask, 2])
+    delivery_earnings = sum(merged_array[loaded_mask, 2])
+    warehouse_penalty = sum(deadline ** 2 for deadline in
+                            merged_array[penalty_mask & remaining_mask, 3]
+                            if deadline < 0)
+    warehouse_packages = len(merged_array[remaining_mask])
+    warehouse_earnings = sum(merged_array[remaining_mask, 2])
             
-        print(f'{remaining_overdue} overdue packages remaining.')
-        print(f'{daily_earnings:.2f} daily earnings.')
-        print(f'{total_packages} packages loaded.')
-    
-    # plt.show()
+    print(f'Earnings in delivery: {delivery_earnings:.2f}')
+    print(f'Penalty in warehouse: {warehouse_penalty:.2f}')
+    print(f'Packages in warehouse: {warehouse_packages:.2f}')
+    print(f'Earnings in warehouse: {warehouse_earnings:.2f}')
+    plt.show()
 
 
 if __name__ == '__main__':
