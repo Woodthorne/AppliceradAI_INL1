20306069d44427f8089ed18c1401d01c65cffaf1
Daniel Alm D'Adda <swedmarine@hotmail.com>
2025-01-21 08:30:55 +0100

    Added docstring

--- seeder.py
+++ seeder.py
@@ -2,9 +2,23 @@ import csv
 import random
 from pathlib import Path
 
-def seed_packages(n_iter: int = 100, target_path: Path = Path('lagerstatus.csv')) -> None:
+def seed_packages(
+        n_iter: int = 100,
+        target_path: Path = Path('lagerstatus.csv')
+) -> None:
+    '''Seeds random packages to file
+    
+    Parameters
+    ----------
+    n_iter: int, default 100
+        Number of packages to generate.
+    target_path: pathlib.Path, default Path('lagerstatus.csv')
+        Path of csv-file to save to. Note that if file already exists
+        it will be overwritten.
+    '''
     assert 0 < n_iter, 'n_iter needs to be a positive integer'
     assert n_iter < 9_000_000_000, 'n_iter needs to be less than 9 billion'
+    assert target_path.suffix == '.csv', f'[{target_path.suffix}] target path needs to be csv-file'
 
     entries = []
     id_num = random.randint(1_000_000_000, 9_999_999_999 - n_iter)
